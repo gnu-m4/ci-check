169b8d521d5b59cea3632c41fa8c1a9a62716adf
Bruno Haible <bruno@clisp.org>
2025-04-14 23:40:00 +0200

    Update from the newest template.

--- build-tarball.sh
+++ build-tarball.sh
@@ -25,7 +25,7 @@ set -e
 
 # Fetch sources (uses package 'git').
 git clone --depth 1 --branch branch-1.4 https://git.savannah.gnu.org/git/"$package".git
-git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git
+git clone --depth 1 "${gnulib_url}"
 
 # Apply patches.
 (cd "$package" && patch -p1 < ../patches/0001-maint-Avoid-a-gcc-13-warning-that-makes-enable-gcc-w.patch)
